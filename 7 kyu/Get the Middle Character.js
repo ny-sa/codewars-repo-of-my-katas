c401dccf190f3e2c0eee2ec68c6926b5c0a00372
nysa <msun698@gmail.com>
2019-12-13 21:24:52 +0200

    ....

--- 7 kyu/Get the Middle Character.js
+++ 7 kyu/Get the Middle Character.js
@@ -23,5 +23,11 @@
 //My solution
 function getMiddle(s)   //No native methods challenge
 {
-    return s.length % 2 ? s[s.length/2 - 0.5] : s[s.length/2 - 1] + s[s.length/2];
+    return s.length % 2 ? s[s.length/2 - 0.5] : s[s.length/2 - 1] + s[s.length/2];  //accesses the middle element directly instead
+}
+
+//'Best Practices' solution
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
 }
