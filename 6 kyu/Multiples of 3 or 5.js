567912158bfd8a988f7e9a183126893793b372a1
nysa <msun698@gmail.com>
2019-12-22 16:16:57 +0200

    .

--- 6 kyu/Multiples of 3 or 5.js
+++ 6 kyu/Multiples of 3 or 5.js
@@ -11,3 +11,15 @@ function solution(number) { //No native methods challenge
         if (i % 3 == 0 || i % 5 == 0) sum += i;
     return sum;
 }
+
+//'Best Practices' solution
+function solution(number){  //effectively the same
+    var sum = 0;
+    
+    for(var i = 1;i< number; i++){
+      if(i % 3 == 0 || i % 5 == 0){
+        sum += i
+      }
+    }
+    return sum;
+  }
